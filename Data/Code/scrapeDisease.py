7edc417f75827f0abfa1b4f54793967bae9be110
stang72 <30294362+stang72@users.noreply.github.com>
2017-07-27 00:07:03 -0400

    Update scrapeDisease.py

--- Data/Code/scrapeDisease.py
+++ Data/Code/scrapeDisease.py
@@ -19,40 +19,53 @@ def getDiseaseName(driver):
     if "We can\'t seem to find the page" in driver.page_source:
         return []
     else:
-        diseaseName=driver.find_elements_by_class_name("charcoal")
+        diseaseName=driver.find_elements_by_class_name('charcoal')
         return diseaseName
 
 def getPage(driver):
     if "We can\'t seem to find the page" in driver.page_source:
         pager=[]
     else:
-        pager=driver.find_elements_by_class_name("pager-item")
+        pager=driver.find_elements_by_class_name('pager-item')
     return pager
 
 def nextPage(driver,pager):
     if (len(pager)!=0):
-        pager[0].click()
-        pager.pop(0)        
-#def getSymptom(elementList):
-    
+        temp_ul=pager[0].find_element_by_class_name('active').get_attribute('href')
+        driver.get(temp_ul)
+        pager.pop(0)
+    else:
+        print "This is the last page!"
+
+def getContent(driver):
+    if "We can\'t seem to find the page" in driver.page_source:
+        content = {}
+    else:
+        #contentEle=driver.find_element_by_class_name('content')
+        content = {}
+    return content
 
 #def getDescription(elementList):
-    
+ 
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome('C:/Users/IrisTang/Documents/zzz/aip/chromedriver_win32/chromedriver.exe')
-    
-    web_catalog = ("http://www.petmd.com/cat/conditions#")
-    start_time = time.time()
-    driver.get(web_catalog)
-    try:
-        diseaseElement = getDiseaseName(driver)
-    except Exception:
-        print "error in getDiseaseName"
-    print("---%s seconds ---" % (time.time() - start_time))
+    driver = webdriver.Chrome('C:/Users/Shi/Desktop/aipet/chromedriver.exe')    
+    web_catalog = ("http://www.petmd.com/cat/conditions#")    
+    driver.get(web_catalog)    
+    diseaseElement = getDiseaseName(driver)   
     diseaseElement.pop(0)
-    print diseaseElement[0].text
-    diseaseElement[0].click()
+    url_list = []
+    for i in range(len(diseaseElement)):
+        url_temp=diseaseElement[i].get_attribute('href')
+        url_list.append(url_temp)
+#--------------------------------test----------------------------------------    
+    ul=url_list[0]
+    driver.get(ul)
+    print driver.find_element_by_id('content-content').find_element_by_class_name('content').text
+    page_n = getPage(driver)
+# Maybe try find element by XPath???    
+# Maybe try manipulating/split text after download the whole page content???
+    
     
     #for element in range(len(diseaseElement)):
         
