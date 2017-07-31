9b912b0b057b93c1dd9e7f299b487c0b7fea9b8a
stang72 <30294362+stang72@users.noreply.github.com>
2017-07-30 21:11:41 -0400

    Update scrapeDisease.py

--- Data/Code/scrapeDisease.py
+++ Data/Code/scrapeDisease.py
@@ -35,17 +35,36 @@ def nextPage(driver,pager):
         driver.get(temp_ul)
         pager.pop(0)
     else:
-        print "This is the last page!"
+        pass
 
-def getContent(driver):
+def getContentDict(driver):
     if "We can\'t seem to find the page" in driver.page_source:
-        content = {}
+        content_dict = {}
     else:
-        #contentEle=driver.find_element_by_class_name('content')
-        content = {}
-    return content
+        content = driver.find_elements_by_css_selector('#content-content > div > div.content > *')
+        content_1=[]        
+        #Create "content_1" to remove page number from text
+	for c in content:
+            if c.tag_name in ['h2','h3','p','ul']:
+                content_1.append(c)
+            else:
+                pass
+        #Get indeces of headings
+	title_indext=[]
+        for i in range(len(content_1)):
+            if (content_1[i].tag_name in ['h2','h3']):
+                title_indext.append(i)
+            else:
+                pass
+        title_indext.append(len(content_1)-1)
+        content_text=[]
+        for c in content_1:
+            content_text.append(c.text)
+        content_dict={}
+        for i in range(len(title_indext)-1):
+            content_dict[content_text[title_indext[i]]] = content_text[title_indext[i]+1:title_indext[i+1]]
+    return content_dict
 
-#def getDescription(elementList):
  
 
 if __name__ == "__main__":
@@ -61,10 +80,7 @@ if __name__ == "__main__":
 #--------------------------------test----------------------------------------    
     ul=url_list[0]
     driver.get(ul)
-    print driver.find_element_by_id('content-content').find_element_by_class_name('content').text
+    getContentDict(driver)
+            
     page_n = getPage(driver)
-# Maybe try find element by XPath???    
-# Maybe try manipulating/split text after download the whole page content???
-        
-#a= driver.find_elements_by_css_selector('#content-content > div > div.content > *')  
-#a[15].tag_name = 'h3'
+    driver.close()
