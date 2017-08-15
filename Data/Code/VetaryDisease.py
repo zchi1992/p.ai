2a8c20e9e3f1a87761f6045669f229277042d896
cs3285 <cs3285@columbia.edu>
2017-08-14 23:26:40 -0400

    Create VetaryDisease.py

---
+++ Data/Code/VetaryDisease.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+import selenium.webdriver.support.expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from string import ascii_uppercase
+
+def getablink():
+    link_list = []
+    for alphabet in ascii_uppercase:
+        link_list.append("https://www.vetary.com/dog/conditions/" + alphabet)
+    return link_list
+
+def getdiseaseLink():
+    diseaseLink_list = []
+    for link in alphabet_link:
+        driver.get(link)
+        elements = driver.find_elements_by_css_selector("body > div.wrap > div.outer-main-body > div > div:nth-child(2) > ul > *")
+        for element in elements:
+            diseaseLink_list.append([elements[0].find_element_by_css_selector('a').text, element.find_element_by_css_selector('a').get_attribute('href')])
+    return diseaseLink_list
+
+body > div.wrap > div.outer-main-body > div > div:nth-child(2) > ul > li:nth-child(1) > a
+if __name__ == "__main__":
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {"profile.managed_default_content_settings.images":2}
+    chromeOptions.add_experimental_option("prefs",prefs)
+    driver = webdriver.Chrome('Z:\\WebDriver\\chromedriver.exe', chrome_options=chromeOptions)
+
+    alphabet_link = getablink()
+
+    diseaseLink_list = getdiseaseLink(alphabet_link)
