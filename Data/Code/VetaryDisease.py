9523feb94ad259e33a403f9ac55b283c21d59e0c
cs3285 <cs3285@columbia.edu>
2017-08-17 18:26:23 -0400

    Update VetaryDisease.py

--- Data/Code/VetaryDisease.py
+++ Data/Code/VetaryDisease.py
@@ -11,7 +11,7 @@ def getablink():
         link_list.append("https://www.vetary.com/dog/conditions/" + alphabet)
     return link_list
 
-def getdiseaseLink():
+def getdiseaseLink(alphabet_link):
     diseaseLink_list = []
     for link in alphabet_link:
         driver.get(link)
@@ -20,12 +20,27 @@ def getdiseaseLink():
             diseaseLink_list.append([elements[0].find_element_by_css_selector('a').text, element.find_element_by_css_selector('a').get_attribute('href')])
     return diseaseLink_list
 
+def GetDiseaseDetail(link):
+    driver.get(link)
+    Symptoms_Key = driver.find_element_by_css_selector("#article-info-wrapper-mobile > div > div.col.article-info-details > div.most-common-symptoms > div > ul").text.replace("\n","/")
+    Introduction = driver.find_elements_by_css_selector("#mobile-reminder-scroll-thresh > div > div.main-content-col > div:nth-child(3) > *")
+
+
 if __name__ == "__main__":
     chromeOptions = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images":2}
     chromeOptions.add_experimental_option("prefs",prefs)
-    driver = webdriver.Chrome('Z:\\WebDriver\\chromedriver.exe', chrome_options=chromeOptions)
+    driver = webdriver.Chrome('C:\\Users\\ChuyaoShen\\Desktop\\pianzi\\chromedriver.exe', chrome_options=chromeOptions)
 
+
+    driver = webdriver.Chrome('C:\\Users\\ChuyaoShen\\Desktop\\pianzi\\chromedriver.exe')
     alphabet_link = getablink()  
 
     diseaseLink_list = getdiseaseLink(alphabet_link)
+
+    clean_data = pd.DataFrame(columns = ["Category", "ID", "Disease", "Symptoms_Key", "Introduction", "Symptoms_Detailed", "Causes", "Diagnosis", "Treatment", "Recovery", "Cost"])
+    for i in diseaseLink_list:
+        Category = "dog"
+        ID = i
+        Disease = diseaseLink_list[i][0]
+        [Symptoms_Key, Introduction, Symptoms_Detailed, Causes, Diagnosis, Treatment, Recovery, Cost] = GetDiseaseDetail(diseaseLink_list[i][1])
